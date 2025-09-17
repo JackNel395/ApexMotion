187064710dee19772b6f525aa53bce1bdfeb58f6
JackNel395 <jnrebias@gmail.com>
2025-09-17 09:15:35 +0800

    second commit

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYGmop2hT3e1Q_OSYVgFc4s5R7XTZa1hg",
+  authDomain: "apexmotion-eed5b.firebaseapp.com",
+  projectId: "apexmotion-eed5b",
+  storageBucket: "apexmotion-eed5b.firebasestorage.app",
+  messagingSenderId: "868428333658",
+  appId: "1:868428333658:web:18dd1265015a258e8c8e63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
